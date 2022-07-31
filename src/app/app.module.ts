0e98de38b2c0be614a3998e938063c8f7cb85220
VysotskaNatali <Bljondinka7@gmail.com>
2022-07-31 20:25:10 +0300

    Angular, Angular CLI, Data Binding Add Cenzor

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CenzorComponent } from './cenzor/cenzor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CenzorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
